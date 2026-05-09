d382d5096991b483768e133620272a4ea5dd469d
kanchang12 <32091338+kanchang12@users.noreply.github.com>
2026-05-09 05:16:40 +0100

    Update main.py

--- main.py
+++ main.py
@@ -239,4 +239,4 @@ def catch_all(path:str): return FileResponse("static/index.html")
 
 if __name__=="__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8080)), reload=False)
